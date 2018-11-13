ba8854a24665160f1793da8e55a6c8bb8cedc753
oleglata <oleglata@gmail.com>
2018-11-13 18:53:11 +0200

    lesson 4

---
+++ ml_lesson4.py
@@ -0,0 +1,37 @@
+
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+df = pd.read_csv('indians-diabetes.csv', names=names)
+
+
+import numpy as np
+def stratified_split(y, proportion=0.8):
+    y = np.array(y)
+
+    train_inds = np.zeros(len(y), dtype=bool)
+    test_inds = np.zeros(len(y), dtype=bool)
+
+    values = np.unique(y)
+    for value in values:
+        value_inds = np.nonzero(y == value)[0]
+        np.random.shuffle(value_inds)
+
+        n = int(proportion * len(value_inds))
+
+        train_inds[value_inds[:n]] = True
+        test_inds[value_inds[n:]] = True
+
+    return train_inds, test_inds
+
+train, test = stratified_split(df['class'])
+X_train = df.iloc[train, 0:8]
+X_test = df.iloc[test, 0:8]
+
+y_train = df['class'][train]
+y_test = df['class'][test]
+
+from sklearn.linear_model import LogisticRegression
+def accuracy(y_test, y_pred):
+    return 1 - sum(abs(y_test - y_pred)/len(y_test))
+
+
+print(accuracy(y_test, y_pred))
--- ml_test1.py
+++ ml_test1.py
@@ -1,2 +1,41 @@
 import pandas as pd
 
+
+titanic_df = pd.read_csv('titanic.csv')
+
+
+
+def norm_arr(arr):
+    mean = arr.mean()
+    std = arr.std()
+
+    normalized = (arr - mean) / std
+    return normalized
+
+
+dat = pd.read_csv('titanic.csv')
+df = pd.DataFrame(dat)
+a=df[df['Age'].notnull()]
+b=a['Age']
+c=norm_arr(b)
+
+def norm_arr(arr):
+    mean = arr.mean()
+    std = arr.std()
+    normalized = (arr - mean) / std
+    return normalized
+dat = pd.read_csv('titanic.csv')
+df = pd.DataFrame(dat)
+a=df[df['Age'].notnull()]
+
+
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+df = pd.read_csv('indians-diabetes.csv', names=names)
+
+import matplotlib.pyplot as plt
+df.boxplot()
+df.hist()
+df.groupby('class').hist()
+
+df.groupby('class').age.hist(alpha=0.4)
+plt.show()
